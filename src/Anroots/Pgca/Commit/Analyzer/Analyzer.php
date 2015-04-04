c7558d86da229d9494c963bc748d4f5ff7834c71
Ando Roots <ando@sqroot.eu>
2015-04-04 13:29:43 +0300

    Delete AbstractAnalyzer class

--- src/Anroots/Pgca/Commit/Analyzer/Analyzer.php
+++ src/Anroots/Pgca/Commit/Analyzer/Analyzer.php
@@ -3,11 +3,103 @@
 namespace Anroots\Pgca\Commit\Analyzer;
 
 use Anroots\Pgca\CollectionSetAwareInterface;
+use Anroots\Pgca\Commit\Provider\CommitProviderInterface;
 use Anroots\Pgca\Git\CommitInterface;
+use Anroots\Pgca\ReportInterface;
 use Anroots\Pgca\Rule\RuleInterface;
+use Anroots\Pgca\Rule\ViolationInterface;
 
-class Analyzer extends AbstractAnalyzer implements CollectionSetAwareInterface
+class Analyzer implements CollectionSetAwareInterface, CommitAnalyzerInterface
 {
+    /**
+     * @var CommitProviderInterface
+     */
+    protected $commitProvider;
+
+    /**
+     * @var RuleInterface[]
+     */
+    protected $rules;
+
+    /**
+     * @var ReportInterface
+     */
+    protected $report;
+
+    /**
+     * @param ReportInterface $report
+     */
+    public function __construct(ReportInterface $report)
+    {
+        $this->report = $report;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function run()
+    {
+        if (!count($this->rules)) {
+            throw new RuleException('No analyzer rules provided');
+        }
+
+        $commits = $this->commitProvider->getCommits();
+
+        foreach ($commits as $commit) {
+            $this->analyzeCommit($commit);
+        }
+    }
+
+
+    public function setCommitProvider(CommitProviderInterface $commitProvider)
+    {
+        $this->commitProvider = $commitProvider;
+
+        return $this;
+    }
+
+    public function addViolation(ViolationInterface $violation)
+    {
+        $this->report->addViolation($violation);
+    }
+
+    /**
+     * @return \Anroots\Pgca\Rule\RuleInterface[]
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param \Anroots\Pgca\Rule\RuleInterface[] $rules
+     * @return $this
+     */
+    public function setRules(array $rules)
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReport()
+    {
+        return $this->report;
+    }
+
+    /**
+     * @param ReportInterface $report
+     * @return $this
+     */
+    public function setReport(ReportInterface $report)
+    {
+        $this->report = $report;
+
+        return $this;
+    }
 
     /**
      * @return mixed
